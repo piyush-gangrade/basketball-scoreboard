d029f1c5a550dd9b477930c082d1e56160249c49
piyush-gangrade <piyushgangrade750@gmail.com>
2024-03-27 22:37:56 +0530

    remove scores btn bug
    scores button is working before game starts. Now its fix

--- index.js
+++ index.js
@@ -7,19 +7,21 @@ const mainBtn = document.querySelector("#main-btn");
 mainBtn.addEventListener("click", gameStart);
 
 scoreBtn.forEach(btn => btn.addEventListener('click',()=>{
-    switch(btn.id){
-        case "firstScoreAddOne": score1.value = parseInt(score1.value)+1;
-        break;
-        case "firstScoreAddTwo": score1.value = parseInt(score1.value)+2;
-        break;
-        case "firstScoreAddThree": score1.value = parseInt(score1.value)+3;
-        break;
-        case "secondScoreAddOne": score2.value = parseInt(score2.value)+1;
-        break;
-        case "secondScoreAddTwo": score2.value = parseInt(score2.value)+2;
-        break;
-        case "secondScoreAddThree": score2.value = parseInt(score2.value)+3;
-        break;
+    if(mainBtn.textContent !== "Start"){
+        switch(btn.id){
+            case "firstScoreAddOne": score1.value = parseInt(score1.value)+1;
+            break;
+            case "firstScoreAddTwo": score1.value = parseInt(score1.value)+2;
+            break;
+            case "firstScoreAddThree": score1.value = parseInt(score1.value)+3;
+            break;
+            case "secondScoreAddOne": score2.value = parseInt(score2.value)+1;
+            break;
+            case "secondScoreAddTwo": score2.value = parseInt(score2.value)+2;
+            break;
+            case "secondScoreAddThree": score2.value = parseInt(score2.value)+3;
+            break;
+        }
     }
 }))
 
